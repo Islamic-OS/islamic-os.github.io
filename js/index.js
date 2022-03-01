ad5883baea194237bf26cbc5dab61bd697fcf1de
Md. Riyasat Hossain <riyasat.hossain.29@gmail.com>
2022-03-01 16:52:29 +0600

    Navbar UI done, but not functional

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
+// Navbar Animations
+gsap.from("nav", {
+  duration: 1,
+  y: -100,
+  opacity: 0,
+  ease: "power3.inOut",
+});
+
+// Header Animations
 gsap.from(
   ["header section.one figure p", "header section.one figure figcaption"],
   {
@@ -6,13 +15,14 @@ gsap.from(
     opacity: 0,
     ease: "power3.out",
     stagger: 1,
+    delay: 1,
   }
 );
 
 gsap.from("header section.two img", {
   duration: 1.5,
-  x: -200,
+  x: 200,
   opacity: 0,
   ease: "power3.out",
-  delay: 2,
+  delay: 3,
 });
