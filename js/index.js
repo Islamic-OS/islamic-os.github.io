fc4bb12bb49aa1a743f5eec2780e2bca338bff53
Md. Riyasat Hossain <riyasat.hossain.29@gmail.com>
2022-03-01 14:07:34 +0600

    Header done, Navbar under construction

---
+++ js/index.js
@@ -0,0 +1,18 @@
+gsap.from(
+  ["header section.one figure p", "header section.one figure figcaption"],
+  {
+    duration: 1.5,
+    y: -100,
+    opacity: 0,
+    ease: "power3.out",
+    stagger: 1,
+  }
+);
+
+gsap.from("header section.two img", {
+  duration: 1.5,
+  x: -200,
+  opacity: 0,
+  ease: "power3.out",
+  delay: 2,
+});
